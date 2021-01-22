29cc11e8adb1b02250de85b808fe37bc56c6bdc6
Juan David Gonzalez <juan.dagore@gmail.com>
2021-01-21 20:27:38 -0500

    Refactor layouts && add loading component

--- src/store/modules/oauth.js
+++ src/store/modules/oauth.js
@@ -13,13 +13,14 @@ export default {
   },
   actions: {
     getToken ({ commit }) {
+      commit('loading/SET_LOADING', true, { root: true })
       oauth.getToken().then(({ data }) => {
         commit('SET_ACCESS_TOKEN', data.access_token)
       }).catch((err) => {
         commit('SET_ACCESS_TOKEN', null)
         console.log('Error OAuth: ', err)
       }).finally(() => {
-        console.log('Done')
+        commit('loading/SET_LOADING', false, { root: true })
       })
     }
   }
